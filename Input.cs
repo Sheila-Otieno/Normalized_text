3ee8b650fe98594e59ebd254be3b97b0b0856ee7
Sheila-Otieno <59034245+Sheila-Otieno@users.noreply.github.com>
2020-11-11 14:00:29 +0100

    Add files via upload

---
+++ Input.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PDFTextExtract
+{
+    public class Input
+    {
+        public String Text { get; set; }
+    }
+}
